66307befec9f14f579791ad562bf4a484f1b144a
Henry Derbyshire <henry@htderbyshire.com>
2022-01-17 10:57:26 +0000

    initial project setup with webpack

---
+++ dist/main.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+const divtest = document.querySelector(".content");
+console.log("hello");
+divtest.textContent = "Hello World!";
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7OztBQUFBO0FBQ0E7QUFDQSIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zdCBkaXZ0ZXN0ID0gZG9jdW1lbnQucXVlcnlTZWxlY3RvcihcIi5jb250ZW50XCIpO1xuY29uc29sZS5sb2coXCJoZWxsb1wiKTtcbmRpdnRlc3QudGV4dENvbnRlbnQgPSBcIkhlbGxvIFdvcmxkIVwiO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const divtest = document.querySelector(".content");
+console.log("hello");
+divtest.textContent = "Hello World!";
